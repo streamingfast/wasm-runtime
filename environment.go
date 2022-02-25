714fdb18bf32574fe27e521c9ca6d761fe06456e
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-02-25 11:26:30 -0500

    Reafactoring environment to remove DefaultEnvironment

--- environment.go
+++ environment.go
@@ -10,6 +10,8 @@ import (
 	"go.uber.org/zap"
 )
 
+var encoding = binary.LittleEndian
+
 type Environment interface {
 	SetMemory(memory *wasmer.Memory)
 	GetMemory() *wasmer.Memory
@@ -18,8 +20,6 @@ type Environment interface {
 	ReadString(offset int32, len int32) (string, error)
 
 	ReadI32(ptr int32) (int32, error)
-	ReadI32s(offset int32) ([]int32, error)
-	ReadStrings(offset int32, len int32) ([]string, error)
 
 	LogSegment(message string, offset int32, length int32)
 	RecordCall(module, function string, params []interface{}, returns interface{})
@@ -29,107 +29,24 @@ type CallRecorder interface {
 	Record(module, name string, params []interface{}, returnValue interface{})
 }
 
-var emptyEnvironment = &DefaultEnvironment{}
-
-type DefaultEnvironment struct {
+type RustEnvironment struct {
 	CallRecorder CallRecorder
 	memory       *wasmer.Memory
 }
 
-type RustEnvironment struct {
-	env *DefaultEnvironment
-}
-
-func NewRustEnvironment(env *DefaultEnvironment) *RustEnvironment {
-	return &RustEnvironment{
-		env: env,
-	}
+func (e *RustEnvironment) SetMemory(memory *wasmer.Memory) {
+	e.memory = memory
 }
 
 func (e *RustEnvironment) GetMemory() *wasmer.Memory {
-	return e.env.memory
-}
-
-func (e *RustEnvironment) SetMemory(memory *wasmer.Memory) {
-	e.env.SetMemory(memory)
-}
-
-func (e *RustEnvironment) ReadBytes(offset int32, length int32) ([]byte, error) {
-	return e.env.ReadBytes(offset, length)
-}
-
-func (e *RustEnvironment) ReadString(offset int32, len int32) (string, error) {
-
-	bytes, err := e.env.segment(offset, len)
-	if err != nil {
-		return "", fmt.Errorf("read content: %w", err)
-	}
-	return string(bytes), nil
-}
-
-func (e *RustEnvironment) ReadI32(offset int32) (int32, error) {
-	return e.env.readI32("", offset)
-}
-
-func (e *RustEnvironment) ReadI32s(offset int32) ([]int32, error) {
-	return e.env.ReadI32s(offset)
-}
-
-func (e *RustEnvironment) ReadStrings(offset int32, len int32) ([]string, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (e *RustEnvironment) LogSegment(message string, offset int32, length int32) {
-	e.env.LogSegment(message, offset, length)
-}
-
-func (e *RustEnvironment) RecordCall(module, function string, params []interface{}, returns interface{}) {
-	e.env.RecordCall(module, function, params, returns)
-}
-
-var encoding = binary.LittleEndian
-var bigEncoding = binary.BigEndian
-
-func (e *DefaultEnvironment) SetMemory(memory *wasmer.Memory) {
-	e.memory = memory
-}
-func (e *DefaultEnvironment) GetMemory() *wasmer.Memory {
 	return e.memory
 }
 
-func (e *DefaultEnvironment) dataAt(offset int32) ([]byte, error) {
-	bytes := e.memory.Data()
-	if offset < 0 {
-		return nil, fmt.Errorf("offset %env must be positive", offset)
-	}
-
-	if offset > int32(len(bytes)) {
-		return nil, fmt.Errorf("offset %env out of memory bounds ending at %env", offset, len(bytes))
-	}
-
-	return e.memory.Data()[offset:], nil
+func (e *RustEnvironment) ReadBytes(offset int32, length int32) ([]byte, error) {
+	return e.segment(offset, length)
 }
 
-func (e *DefaultEnvironment) segment(offset int32, length int32) ([]byte, error) {
-	bytes := e.memory.Data()
-	if offset < 0 {
-		return nil, fmt.Errorf("offset %env must be positive", offset)
-	}
-
-	if offset >= int32(len(bytes)) {
-		return nil, fmt.Errorf("offset %env out of memory bounds ending at %env", offset, len(bytes))
-	}
-
-	end := offset + length
-	if end > int32(len(bytes)) {
-		return nil, fmt.Errorf("end %env out of memory bounds ending at %env", end, len(bytes))
-	}
-
-	return bytes[offset : offset+length], nil
-}
-
-func (e *DefaultEnvironment) ReadString(offset int32, _ int32) (string, error) {
+func (e *RustEnvironment) ReadString(offset int32, len int32) (string, error) {
 	e.LogSegment("Data +size type?", offset-12, 16)
 
 	characterCount, err := e.readI32("string length", offset)
@@ -156,7 +73,11 @@ func (e *DefaultEnvironment) ReadString(offset int32, _ int32) (string, error) {
 	return string(utf16.Decode(characters)), nil
 }
 
-func (e *DefaultEnvironment) LogSegment(message string, offset int32, length int32) {
+func (e *RustEnvironment) ReadI32(offset int32) (int32, error) {
+	return e.readI32("", offset)
+}
+
+func (e *RustEnvironment) LogSegment(message string, offset int32, length int32) {
 	// a82700000000000003000000000000000300000000000000
 	// e6f5af
 
@@ -171,81 +92,21 @@ func (e *DefaultEnvironment) LogSegment(message string, offset int32, length int
 	}
 }
 
-func (e *DefaultEnvironment) ReadBytes(offset int32, length int32) (out []byte, err error) {
-	return e.segment(offset, length)
+func (e *RustEnvironment) RecordCall(module, function string, params []interface{}, returns interface{}) {
+	if e.CallRecorder != nil {
+		e.CallRecorder.Record(module, function, params, returns)
+	}
 }
 
-func (e *DefaultEnvironment) ReadI32s(offset int32) (out []int32, err error) {
-	arrayOffset, err := e.readI32("i32 array offset", offset)
-	if err != nil {
-		return nil, fmt.Errorf("read i32 array offset: %w", err)
+func (e *RustEnvironment) Debug() string {
+	if e.memory == nil {
+		return "<empty>"
 	}
 
-	length, err := e.readI32("i32 array length", offset+4)
-	if err != nil {
-		return nil, fmt.Errorf("read i32 array length: %w", err)
-	}
-
-	if ztracer.Enabled() {
-		zlog.Debug("resolving i32 array reference", zap.Int32("offset", arrayOffset), zap.Int32("length", length))
-	}
-
-	// Gives 0800000000000000 (0000000000000008 in big endian), not sure of the meaning actually
-	_, err = e.readI64("i32 array field", arrayOffset)
-
-	indicesOffset := arrayOffset + 8
-	sizeOfI32 := int32(4)
-	out = make([]int32, length)
-	for i := int32(0); i < length; i++ {
-		out[i], err = e.readI32("i32 array element", indicesOffset+(i*sizeOfI32))
-		if err != nil {
-			return nil, fmt.Errorf("read i32 array index #%env: %w", i, err)
-		}
-	}
-
-	return out, nil
+	return hex.EncodeToString(e.memory.Data())
 }
 
-func (e *DefaultEnvironment) ReadStrings(offset int32, _ int32) (out []string, err error) {
-	arrayOffset, err := e.readI32("string array offset", offset)
-	if err != nil {
-		return nil, fmt.Errorf("read string array offset: %w", err)
-	}
-
-	length, err := e.readI32("string array length", offset+4)
-	if err != nil {
-		return nil, fmt.Errorf("read string array length: %w", err)
-	}
-
-	if ztracer.Enabled() {
-		zlog.Debug("resolving string array reference", zap.Int32("offset", arrayOffset), zap.Int32("length", length))
-	}
-
-	// Gives 0800000000000000 (0000000000000008 in big endian), not sure of the meaning actually
-	_, err = e.readI64("string array field", arrayOffset)
-
-	indicesOffset := arrayOffset + 8
-	sizeOfString := int32(4)
-	out = make([]string, length)
-	for i := int32(0); i < length; i++ {
-		stringOffset, err := e.readI32("string array element offset", indicesOffset+(i*sizeOfString))
-		if err != nil {
-			return nil, fmt.Errorf("read string array index #%env offset: %w", i, err)
-		}
-
-		out[i], err = e.ReadString(stringOffset, 0)
-		if err != nil {
-			return nil, fmt.Errorf("read string array index #%env: %w", i, err)
-		}
-	}
-
-	return out, nil
-}
-
-func (e *DefaultEnvironment) ReadI32(offset int32) (int32, error) {
-	return e.readI32("", offset)
-}
-func (e *DefaultEnvironment) readI32(tag string, offset int32) (int32, error) {
+func (e *RustEnvironment) readI32(tag string, offset int32) (int32, error) {
 	bytes, err := e.segment(offset, 4)
 	if err != nil {
 		return 0, err
@@ -261,7 +122,38 @@ func (e *DefaultEnvironment) readI32(tag string, offset int32) (int32, error) {
 	return out, nil
 }
 
-func (e *DefaultEnvironment) readU32(tag string, offset int32) (uint32, error) {
+func (e *RustEnvironment) dataAt(offset int32) ([]byte, error) {
+	bytes := e.memory.Data()
+	if offset < 0 {
+		return nil, fmt.Errorf("offset %d env must be positive", offset)
+	}
+
+	if offset > int32(len(bytes)) {
+		return nil, fmt.Errorf("offset %d env out of memory bounds ending at %d env", offset, len(bytes))
+	}
+
+	return e.memory.Data()[offset:], nil
+}
+
+func (e *RustEnvironment) segment(offset int32, length int32) ([]byte, error) {
+	bytes := e.memory.Data()
+	if offset < 0 {
+		return nil, fmt.Errorf("offset %d env must be positive", offset)
+	}
+
+	if offset >= int32(len(bytes)) {
+		return nil, fmt.Errorf("offset %d env out of memory bounds ending at %d env", offset, len(bytes))
+	}
+
+	end := offset + length
+	if end > int32(len(bytes)) {
+		return nil, fmt.Errorf("end %d env out of memory bounds ending at %d env", end, len(bytes))
+	}
+
+	return bytes[offset : offset+length], nil
+}
+
+func (e *RustEnvironment) readU32(tag string, offset int32) (uint32, error) {
 	bytes, err := e.segment(offset, 4)
 	if err != nil {
 		return 0, err
@@ -275,7 +167,7 @@ func (e *DefaultEnvironment) readU32(tag string, offset int32) (uint32, error) {
 	return out, nil
 }
 
-func (e *DefaultEnvironment) readI64(tag string, offset int32) (int64, error) {
+func (e *RustEnvironment) readI64(tag string, offset int32) (int64, error) {
 	bytes, err := e.segment(offset, 8)
 	if err != nil {
 		return 0, err
@@ -289,18 +181,4 @@ func (e *DefaultEnvironment) readI64(tag string, offset int32) (int64, error) {
 	}
 
 	return out, nil
-}
-
-func (e *DefaultEnvironment) RecordCall(module, function string, params []interface{}, returns interface{}) {
-	if e.CallRecorder != nil {
-		e.CallRecorder.Record(module, function, params, returns)
-	}
-}
-
-func (e *DefaultEnvironment) Debug() string {
-	if e.memory == nil {
-		return "<empty>"
-	}
-
-	return hex.EncodeToString(e.memory.Data())
 }
