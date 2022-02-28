d81decafcbd3066b9244ea98383380ebe0428839
billettc <charles@cbillette.com>
2022-02-28 09:31:33 -0500

    wip

--- testing/rust_scripts/big_bytes/src/lib.rs
+++ testing/rust_scripts/big_bytes/src/lib.rs
@@ -1,22 +1,26 @@
-use std::slice;
 extern {
     fn println(ptr: *const u8, len: usize);
 }
 
 #[no_mangle]
-pub extern "C" fn read_big_bytes(ptr: *const u8, len: usize, output: &mut (*const u8, usize))  {
+pub extern "C" fn read_big_bytes(ptr: *mut u8, len: usize, output: &mut (*const u8, usize))  {
     unsafe {
         let ptr_info = format!("WTF");
         println(ptr_info.as_ptr(), ptr_info.len());
     }
 
-    let slice = unsafe { slice::from_raw_parts(ptr as _, len as _) };
     unsafe {
-        let ptr_info = format!("slice info {:?} ", slice.len());
+        let mut input_data = Vec::from_raw_parts(ptr, len, len);
+        let mut input_ptr = input_data.as_mut_ptr();
+        input_ptr[1] = 2;
+        let ptr_info = format!("slice info {:?} {:?} {:?}", input_ptr, input_data.len(), input_ptr[1]);
         println(ptr_info.as_ptr(), ptr_info.len());
+        output.0 = input_ptr.as_ptr();
+        output.1 = input_data.len();
+        let done = format!("all done!");
+        println(done.as_ptr(), done.len());
     }
 
-    output.0 = slice.as_ptr();
-    output.1 = slice.len();
+    // slice[1] = 2;
 
 }
